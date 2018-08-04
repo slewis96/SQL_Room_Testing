3f3306dcb38d48489a6a167c6fe39830782cdac9
slewis96 <seblewis96@gmail.com>
2018-08-04 21:43:23 +0100

    Test tests

--- RoomBookingSQLTests/RoomBookingSQLTests/Rooms.cs
+++ RoomBookingSQLTests/RoomBookingSQLTests/Rooms.cs
@@ -1,14 +1,37 @@
 using System;
 using Xunit;
+using Npgsql;
 
 namespace RoomBookingSQLTests
 {
     public class Rooms
     {
+        public NpgsqlConnection conn = new NpgsqlConnection("Server = 127.0.0.1; User Id = postgres;" + "Password=;Database=guitars;");
+        
         [Fact]
-        public void Test1()
+        public void StringReturnName()
         {
-           // start
+            conn.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand("SELECT name FROM guitars", conn);
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read()) {
+                var entry = dr[0];
+                Assert.IsType(Type.GetType("System.String"), entry);
+            }
+            conn.Close();
+        }
+        [Fact]
+        public void IntReturnID()
+        {
+            conn.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand("SELECT id FROM guitars", conn);
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                var entry = dr[0];
+                Assert.IsType(Type.GetType("System.Int32"), entry);
+            }
+            conn.Close();
         }
     }
 }
