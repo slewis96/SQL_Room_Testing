727cc94c506c8be76117c53f436899d5d0885a4e
slewis96 <seblewis96@gmail.com>
2018-08-04 21:11:46 +0100

    Base

---
+++ RoomBookingSQLTests/RoomBookingSQLTests/Rooms.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace RoomBookingSQLTests
+{
+    public class Rooms
+    {
+        [Fact]
+        public void Test1()
+        {
+           // start
+        }
+    }
+}
