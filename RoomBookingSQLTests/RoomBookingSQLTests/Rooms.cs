92902a0788aca98282c14034653bcc08b71de240
Toweka Malcolm <tech-a58@TECH-A58s-MacBook-Air.local>
2018-08-06 14:38:33 +0100

    importance test

--- RoomBookingSQLTests/RoomBookingSQLTests/Rooms.cs
+++ RoomBookingSQLTests/RoomBookingSQLTests/Rooms.cs
@@ -7,33 +7,35 @@ namespace RoomBookingSQLTests
 {
     public class Rooms
     {
-        public NpgsqlConnection conn = new NpgsqlConnection("Server = 127.0.0.1; User Id = postgres;" + "Password=;Database=guitars;");
+        public NpgsqlConnection conn = new NpgsqlConnection("Server = 127.0.0.1; User Id = postgres;" + "Password=;Database=bookingsystem_development;");
 
-        [Fact]
-        public void StringReturnName()
-        {
-            conn.Open();
-            NpgsqlCommand cmd = new NpgsqlCommand("SELECT name FROM guitars", conn);
-            NpgsqlDataReader dr = cmd.ExecuteReader();
-            while (dr.Read()) {
-                var entry = dr[0];
-                Assert.IsType(Type.GetType("System.String"), entry);
-                Console.WriteLine();
-            }
-            conn.Close();
-        }
         [Fact]
         public void IntReturnID()
         {
             conn.Open();
-            NpgsqlCommand cmd = new NpgsqlCommand("SELECT id FROM guitars", conn);
+            NpgsqlCommand cmd = new NpgsqlCommand("SELECT id FROM bookings ORDER BY status DESC LIMIT 20", conn);
             NpgsqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read()) {
+                var entry = dr[0];
+                Assert.IsType(Type.GetType("System.Int64"), entry);
+            }
+            conn.Close();
+        }
+
+        [Fact]
+        public void CharReturnImportance()
+        {
+            conn.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand("SELECT importance FROM bookings ORDER BY status DESC LIMIT 20", conn);
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+            string[] importance = { "!", "!!", "!!!" };
             while (dr.Read())
             {
                 var entry = dr[0];
-                Assert.IsType(Type.GetType("System.Int32"), entry);
+                Assert.Contains(entry, importance);
             }
             conn.Close();
+
         }
     }
 }
