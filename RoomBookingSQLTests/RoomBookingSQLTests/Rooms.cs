99d829f6541ae0ca48724736e8173a07fa372a40
slewis96 <seblewis96@gmail.com>
2018-08-06 14:35:06 +0100

    Category test

--- RoomBookingSQLTests/RoomBookingSQLTests/Rooms.cs
+++ RoomBookingSQLTests/RoomBookingSQLTests/Rooms.cs
@@ -7,31 +7,32 @@ namespace RoomBookingSQLTests
 {
     public class Rooms
     {
-        public NpgsqlConnection conn = new NpgsqlConnection("Server = 127.0.0.1; User Id = postgres;" + "Password=;Database=guitars;");
+        public NpgsqlConnection conn = new NpgsqlConnection("Server = 127.0.0.1; User Id = postgres;" + "Password=;Database=bookingsystem_development;");
 
-        [Fact]
-        public void StringReturnName()
-        {
-            conn.Open();
-            NpgsqlCommand cmd = new NpgsqlCommand("SELECT name FROM guitars", conn);
-            NpgsqlDataReader dr = cmd.ExecuteReader();
-            while (dr.Read()) {
-                var entry = dr[0];
-                Assert.IsType(Type.GetType("System.String"), entry);
-                Console.WriteLine();
-            }
-            conn.Close();
-        }
         [Fact]
         public void IntReturnID()
         {
             conn.Open();
-            NpgsqlCommand cmd = new NpgsqlCommand("SELECT id FROM guitars", conn);
+            NpgsqlCommand cmd = new NpgsqlCommand("SELECT id FROM bookings ORDER BY status DESC LIMIT 20", conn);
             NpgsqlDataReader dr = cmd.ExecuteReader();
             while (dr.Read())
             {
                 var entry = dr[0];
-                Assert.IsType(Type.GetType("System.Int32"), entry);
+                Assert.IsType(Type.GetType("System.Int64"), entry);
+            }
+            conn.Close();
+        }
+        [Fact]
+        public void CategoryPredefined()
+        {
+            conn.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand("SELECT category FROM bookings ORDER BY status DESC LIMIT 20", conn);
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+            string[] precat = { "Face-to-Face Interview", "Phone Interview", "Mock Exams", "Exams", "Presentation Rehersal", "1-On-1", "Client Visit", "Management Meeting", "Sparta Day", "Other" };
+            while (dr.Read())
+            {
+                var entry = dr[0];
+                Assert.Contains(entry, precat);
             }
             conn.Close();
         }
